ef25c7ba5b097ba45fde36546d5b24decd90bf31
Jagadeeshboi <jagadeeshboina@gmail.com>
2024-12-23 09:44:46 +0530

    NewCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Sidebar from './Components/Sidebar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SigninPage } from './Components/SigninPage';
+import Error from './Components/Error';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Sidebar />} />
         <Route path="/sign-in" element={<SigninPage />} />
+        <Route path='*' element={<Error/>}/>
       </Routes>
     </BrowserRouter>
   );
